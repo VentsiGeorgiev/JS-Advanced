630850c3aa4ed6a9250a5ade83900f3caefc73f9
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-02-06 11:48:43 +0100

    d

--- 07. Unit Testing and Error Handling/demo/demoo.js
+++
@@ -1,9 +0,0 @@
-const { sum } = require('./myModule');
-
-describe('Demo Test', () => {
-  it('works with 5 and 3', () => {
-    if (sum(5, 3) != 8) {
-      throw Error('Did not work as expected');
-    }
-  });
-});
--- 07.01. Unit Testing and Error Handling/5/app.js
+++
@@ -1,9 +0,0 @@
-function notify(message) {
-  let notification = document.getElementById('notification');
-  notification.innerText = message;
-  notification.style.display = 'block';
-
-  notification.addEventListener('click', (ev) => {
-    ev.target.style.display = 'none';
-  })
-}
